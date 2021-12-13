f9197dbabf989b0d56b7e343a5cac4adf6d512a3
Huy Dang <huydhm@gmail.com>
2021-12-13 21:17:23 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}","./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['"Inter"', 'sans-serif'],
+        'lexend-deca': ['"Lexend Deca"', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
